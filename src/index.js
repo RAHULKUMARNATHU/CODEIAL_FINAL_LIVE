2282ce86a5a8843ba3562502181135dcba7740e7
Rahul <rahulvaidya148@gmail.com>
2022-05-08 07:29:08 +0530

    Added store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import { configureStore } from './store';
+
+const store = configureStore();
+console.log('store', store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
     <App />
   </React.StrictMode>
+ 
 );
