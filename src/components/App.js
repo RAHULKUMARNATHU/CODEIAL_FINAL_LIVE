d48854edad046ad625bc8b2102dd238cf78ac77c
Rahul <rahulvaidya148@gmail.com>
2022-05-08 12:56:07 +0530

    Fetching Posts from API

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-  return <div className="App">App</div>;
+import { fetchPosts } from '../actions/posts';
+
+class App extends React.Component {
+
+  componentDidMount() { 
+    this.props.dispatch(fetchPosts());
+   }
+
+  render() {
+    console.log('Props' ,this.props);
+    return (
+      <div>App</div>
+    )
+  }
 }
-
-export default App;
+function mapStateToProps(state){
+  return{
+    posts: state.posts,
+  };
+}
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
 import { configureStore } from './store';
 
 const store = configureStore();
-console.log('store', store.getState());
+// console.log('store', store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
- 
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
