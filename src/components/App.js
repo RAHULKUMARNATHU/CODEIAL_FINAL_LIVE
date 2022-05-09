8cde96c2ac8d516dfb8f959ce1cdb2b48f3c944a
Rahul <rahulvaidya148@gmail.com>
2022-05-09 12:25:11 +0530

    Add react router -1

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
 import PropTypes from 'prop-types';
 import { fetchPosts } from '../actions/posts';
-import {PostsList,Navbar} from './'
+import { PostsList, Navbar } from './';
+import { Routes } from 'react-router-dom';
 
+const Login = () => <div> Login </div>;
+const Signup = () => <div>SignUp</div>;
+const Home = () => <div>Home</div>;
 
 class App extends React.Component {
   componentDidMount() {
@@ -14,10 +20,31 @@ class App extends React.Component {
     const { posts } = this.props;
 
     return (
-      <div>
-      <Navbar />
-      <PostsList posts={posts}/>
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+
+          {/* <PostsList posts={posts}/> */}
+
+          <ul>
+          <li>
+          <Link to ="/">Home</Link> 
+         </li>
+         <li>
+          <Link to ="/login">Login</Link> 
+         </li>
+           <li>
+          <Link to ="/signup">SignUp</Link> 
+         </li>
+         </ul>
+
+          <Routes>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+          </Routes>
+        </div>
+      </Router>
     );
   }
 }
@@ -33,3 +60,4 @@ App.propTypes = {
 };
 
 export default connect(mapStateToProps)(App);
+
